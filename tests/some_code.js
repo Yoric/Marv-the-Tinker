924e32472fa1daf192ff062b81dfa03791717668
David Rajchenbach-Teller <dteller@mozilla.com>
2012-03-12 11:42:54 +0100

    [Test] some_code.js: Now with actual directives

---
+++ tests/some_code.js
@@ -0,0 +1,41 @@
+/*
+ * Some non-directive comments
+ */
+/**
+ * Some directive comments for the program itself
+ *
+ * @program {something} else
+ */
+/*
+ * Some more non-directive comments
+ */
+// And yet more
+//* Now, a few directive comments @type {foobar} sna
+
+/**
+ * Directive comments for a var declaration
+ * @program {abc} def
+ * @program {ghi} klm
+ */
+var a;
+
+/**
+ * Directive comments for a fun declaration
+ * @function {nop} qrs
+ * @function {tuv} wxy
+ */
+function b() {
+  
+}
+
+/**
+ * Directive comments for code
+ * @param {zAB} cde
+ */
+print("foo");
+
+/**
+ * Unattached directives
+ */
+
+print("bar");
